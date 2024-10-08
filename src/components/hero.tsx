2564878df23b40b44ab87e94848142bc8b58ac4b
Pancham <{panchamb63@gmail.com}+{Pancham555}@users.noreply.github.com>
2024-10-08 17:00:03 +0530

    Added logo to website

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -36,9 +36,9 @@ export default function Hero() {
           React.js, React Native, Redux.js, Next.js, Three.js, Node.js, MongoDB,
           SQL, Postgresql, Mongoose.js, PostgresSQL, Express.js, Strapi, Git,
           Github, Python, Flask, Django, Django Rest, Scikit-Learn, Numpy,
-          Pandas, Matplotlib, Seaborn, Linux commands, PHP etc and is on it's
-          way to learn several other technologies like the tensorflow in the
-          near future!
+          Pandas, Matplotlib, Seaborn, Linux commands, PHP etc and is on
+          it&apos;s way to learn several other technologies like the tensorflow
+          in the near future!
         </CardDescription>
       </CardContent>
     </Card>
